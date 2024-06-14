e6b35f12d828f1b56053f14747e736363c0f801d
VAL PIMENTEL <113147230+Valinsky-Pimentel@users.noreply.github.com>
2024-06-14 17:02:49 -0600

    Create voting.dart

---
+++ app_votaciones/lib/models/voting.dart
@@ -0,0 +1,9 @@
+import 'candidate.dart';
+
+class Voting {
+  final List<Candidate> candidates;
+
+  Voting({required this.candidates});
+
+  // Puedes agregar más propiedades y métodos según sea necesario
+}
